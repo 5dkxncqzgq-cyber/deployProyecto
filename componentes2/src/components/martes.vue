64ea28de2891f3fc8bd84dfaaba7c93a4cfc1c68
5dkxncqzgq-cyber <5dkxncqzgq@privaterelay.appleid.com>
2026-05-12 22:20:10 -0300

    martes12

--- componentes2/src/components/martes.vue
+++ componentes2/src/components/martes.vue
@@ -1,7 +1,7 @@
 <template>
-<h1> HOLA </h1>
+<h1> HOLA {{MI NOMBRE}} </h1>
 <button v-on:click="decrementa" >-</button>
-<span :class="contColor" id="contador"></span>
-
-
+<span :class="contColor" id="contador">{{contador}}</span>
+<buttonv-on:clock="incrementa">+</button>
+<SumBoton>Material button</SumBoton>
 </template>
